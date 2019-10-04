8ee1d1c8647705906849d6daec9de5d1ef35528d
Ichisada Shioko <ichisadashioko@gmail.com>
2019-10-04 18:10:50 +0700

    add CAStudent

---
+++ CAStudentMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAStudentMVC.MvcApplication" Language="C#" %>
---
+++ CAStudentMVC/Models/CAStudentDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using StudentLib.Models;
+
+namespace CAStudentMVC.Models
+{
+    public class CAStudentDBContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
