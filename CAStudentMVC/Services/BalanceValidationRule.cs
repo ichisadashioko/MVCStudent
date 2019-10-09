2ed9d441fa34ad522d8a769b937c1ea3a669c8cb
Ichisada Shioko <ichisadashioko@gmail.com>
2019-10-09 10:58:52 +0700

    add balance validation rule

---
+++ CAStudentMVC/Services/BalanceValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StudentLib.Models;
+using StudentLib.Services;
+using CAStudentMVC.Models;
+
+namespace CAStudentMVC.Services
+{
+    public class BalanceValidationRule : IValidationRule
+    {
+        public int Order => 15;
+
+        public bool Validate(Student student)
+        {
+            return student.BankBalance >= 500_000_000m;
+        }
+    }
+}
