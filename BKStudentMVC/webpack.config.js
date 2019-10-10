911ae1ea840dde424c227282129be71f6d66239b
Ichisada Shioko <ichisadashioko@gmail.com>
2019-10-10 17:58:09 +0700

    add react env

---
+++ BKStudentMVC/webpack.config.js
@@ -0,0 +1,26 @@
+"use strict"
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: "./Scripts/Home/react/index.js",
+    output: {
+        path: path.resolve(__dirname, "./Scripts/dist/Home/react"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
